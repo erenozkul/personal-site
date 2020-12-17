677eb5a0061965c52cb05414a9d50ac63bb275c8
Eren Özkul <eren.ozkul@dnatech.io>
2020-12-17 15:38:09 +0300

    first commit

---
+++ app/data/contact.js
@@ -0,0 +1,41 @@
+import faGithub from '@fortawesome/fontawesome-free-brands/faGithub';
+import faFacebook from '@fortawesome/fontawesome-free-brands/faFacebookF';
+import faInstagram from '@fortawesome/fontawesome-free-brands/faInstagram';
+import faLinkedinIn from '@fortawesome/fontawesome-free-brands/faLinkedinIn';
+import faTwitter from '@fortawesome/fontawesome-free-brands/faTwitter';
+import faEnvelope from '@fortawesome/fontawesome-free-regular/faEnvelope';
+
+const data = [
+  {
+    link: 'https://github.com/erenozkul',
+    label: 'Github',
+    icon: faGithub,
+  },
+  {
+    link: 'https://facebook.com/erenozkulnet',
+    label: 'Facebook',
+    icon: faFacebook,
+  },
+  {
+    link: 'https://www.instagram.com/erenozkulnet/',
+    label: 'Instagram',
+    icon: faInstagram,
+  },
+  {
+    link: 'https://www.linkedin.com/in/erenozkul',
+    label: 'LinkedIn',
+    icon: faLinkedinIn,
+  },
+  {
+    link: 'https://twitter.com/erenozkulnet',
+    label: 'Twitter',
+    icon: faTwitter,
+  },
+  {
+    link: 'mailto:info@erenozkul.net',
+    label: 'Email',
+    icon: faEnvelope,
+  },
+];
+
+export default data;
---
+++ test/helpers.js
@@ -0,0 +1,35 @@
+const randomString = (length) => {
+  const x = 36 ** (length + 1);
+  const y = 36 ** length;
+  return Math.round(x - (Math.random() * y)).toString(36).slice(1);
+};
+
+const pages = [
+  {
+    route: '/',
+    title: 'Eren Özkul',
+    heading: 'ABOUT THIS SITE',
+  },
+  {
+    route: '/about',
+    title: 'About | Eren Özkul',
+    heading: 'ABOUT ME',
+  },
+  {
+    route: '/projects',
+    title: 'Projects | Eren Özkul',
+    heading: 'PROJECTS',
+  },
+  {
+    route: '/stats',
+    title: 'Stats | Eren Özkul',
+    heading: 'STATS',
+  },
+  {
+    route: '/contact',
+    title: 'Contact | Eren Özkul',
+    heading: 'CONTACT',
+  },
+];
+
+export { pages, randomString };
