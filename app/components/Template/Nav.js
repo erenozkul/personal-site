dbafb151d6548f37dcb56cd419d583eeab87a3cc
Eren Özkul <erain.ozkul@gmail.com>
2021-09-30 01:24:57 +0300

    Update Nav.js

--- app/components/Template/Nav.js
+++ app/components/Template/Nav.js
@@ -100,18 +100,12 @@ const Nav = () => {
       <section className="blurb">
         <h2>About</h2>
         <p>
-          Hi, I&apos;m Eren. I like building things. 
-          I am a <a rel="nofollow" href="https://www.anadolu.edu.tr/">Anadolu University</a> graduate. 
-          The software manager of <a rel="nofollow" href="https://brand-wallet.com">BrandWallet</a> and <a rel="nofollow" href="https://dnatech.io/">DNA</a>. 
+          Hi, I&apos;m Eren. I like building things.
+          I am a <a rel="nofollow" href="https://www.anadolu.edu.tr/">Anadolu University</a> graduate.
+          The software manager of <a rel="nofollow" href="https://brand-wallet.com">BrandWallet</a> and <a rel="nofollow" href="https://dnatech.io/">DNA</a>.
           Before BrandWallet I was at <a rel="nofollow" href="http://www.pennabilisim.com/">Penna</a> and <a rel="nofollow" href="http://mobilizzy.com/">Mobilizzy</a>.
         </p>
-        <ul className="actions">
-          <li>
-            {/*window.location.pathname !== `${BASE_PATH}/resume` ? <Link to="/resume" className="button">Learn More</Link> : <Link to="/about" className="button">About Me</Link>*/}
-          </li>
-        </ul>
       </section>
-
       <section id="footer">
         <ul className="icons">
           {data.map((s) => (
@@ -129,9 +123,3 @@ const Nav = () => {
 };
 
 export default Nav;
-
-
-
-
-/*<p><a style={{ textTransform: 'none' }} href="mailto:info@erenozkul.net">info@erenozkul.net</a></p>
-       */
