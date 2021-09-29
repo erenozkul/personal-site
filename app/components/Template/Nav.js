e8c89b9ea09af15fd24da379f766259d35f7f3ed
Eren Özkul <erain.ozkul@gmail.com>
2021-09-30 01:22:15 +0300

    Update Nav.js

--- app/components/Template/Nav.js
+++ app/components/Template/Nav.js
@@ -99,8 +99,12 @@ const Nav = () => {
 
       <section className="blurb">
         <h2>About</h2>
-        <p>Hi, I&apos;m Eren. I like building things. I am a <a rel="nofollow" href="https://www.anadolu.edu.tr/">Anadolu University</a> graduate. The software manager of <a rel="nofollow" href="https://brand-wallet.com">BrandWallet</a> and <a rel="nofollow" href="https://dnatech.io/">DNA</a>. Before BrandWallet I was at <a rel="nofollow" href="http://www.pennabilisim.com/">Penna</a> and <a rel="nofollow" href="http://mobilizzy.com/">Mobilizzy</a>.
-      </p>
+        <p>
+          Hi, I&apos;m Eren. I like building things. 
+          I am a <a rel="nofollow" href="https://www.anadolu.edu.tr/">Anadolu University</a> graduate. 
+          The software manager of <a rel="nofollow" href="https://brand-wallet.com">BrandWallet</a> and <a rel="nofollow" href="https://dnatech.io/">DNA</a>. 
+          Before BrandWallet I was at <a rel="nofollow" href="http://www.pennabilisim.com/">Penna</a> and <a rel="nofollow" href="http://mobilizzy.com/">Mobilizzy</a>.
+        </p>
         <ul className="actions">
           <li>
             {/*window.location.pathname !== `${BASE_PATH}/resume` ? <Link to="/resume" className="button">Learn More</Link> : <Link to="/about" className="button">About Me</Link>*/}
